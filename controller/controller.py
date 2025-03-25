8021d0cc9fed457438be730a657406a9ff482a5a
shaneydlo <shane.longhurst@gmail.com>
2025-03-25 15:04:58 -0400

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -6,11 +6,8 @@ from signal import pause
 Device.pin_factory = LgpioFactory()
 
 # Define button inputs on GPIO pins 5 and 6 (using BCM numbering)
-button_pin5 = Button(5, pull_up=True, bounce_time=0.2)
-button_pin6 = Button(6, pull_up=True, bounce_time=0.2)
-P2_GPIO = Button(13, pull_up=True, bounce_time=0.2)
-P3_GPIO = Button(12, pull_up=True, bounce_time=0.2)
-P4_GPIO = Button(20, pull_up=True, bounce_time=0.2)
+button_pin5 = Button(5, pull_up=True)#, bounce_time=0.2)
+button_pin6 = Button(6, pull_up=True)#, bounce_time=0.2)
 
 # Define callback functions for when the buttons are pressed
 def on_button5_pressed():
@@ -19,9 +16,14 @@ def on_button5_pressed():
 def on_button6_pressed():
     print("Button on GPIO 6 pressed")
 
-# Attach the callbacks to the button events
-button_pin5.when_pressed = on_button5_pressed
-button_pin6.when_pressed = on_button6_pressed
+print("Polling button states. (Idle state should be 1; pressed state should be 0)")
+while True:
+    print("Button 5:", button5.value, "Button 6:", button6.value)
+    time.sleep(0.5)
 
-print("Monitoring button inputs on GPIO 5 and 6. Press Ctrl+C to exit.")
-pause()
+# Attach the callbacks to the button events
+#button_pin5.when_pressed = on_button5_pressed
+#button_pin6.when_pressed = on_button6_pressed
+
+#print("Monitoring button inputs on GPIO 5 and 6. Press Ctrl+C to exit.")
+#pause()
