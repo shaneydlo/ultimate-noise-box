9dd81202073607277549b33100ec2ffd4fb56e07
shaneydlo <shane.longhurst@gmail.com>
2025-03-26 06:59:54 -0400

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -14,33 +14,6 @@ P2_GPIO = 13
 P3_GPIO = 12 # was 19
 P4_GPIO = 20
 STOP_GPIO = 5
-def button_preset(channel):
-    #
-    # play a preset
-    #
-    # get preset number (1-4) from channel
-    preset_channel = [P1_GPIO, P2_GPIO, P3_GPIO, P4_GPIO]
-    presety = preset_channel.index(channel)
-    # get preset name value from redis
-    r = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
-    p = r.get("p" + str(presety + 1))
-    print("Preset {0} ({1}) pressed.".format(p, presety))
-    # play/display the file image
-    if p is None:
-        print("No preset value for {}".format(presety))
-    else:
-       # text_icon = ["\U000F03A4", "\U000F03A7", "\U000F03AA", "\U000F03AD"]
-        play_file(p, text_icon[presety])
-        
-def button_stop(channel):
-    #
-    # stop payback
-    #
-    global now_playing
-    r = requests.post('http://noise:5000/stop/')
-    print("Stop button pressed.")
-    #now_playing = ""
-    #display_now(idle_image)
 
 def button_preset(channel):
     #
@@ -58,7 +31,28 @@ def button_preset(channel):
         print("No preset value for {}".format(presety))
     else:
         text_icon = ["\U000F03A4", "\U000F03A7", "\U000F03AA", "\U000F03AD"]
-        play_file(p, text_icon[presety])  
+        play_file(p, text_icon[presety])
+        
+def button_stop(channel):
+    #
+    # stop payback
+    #
+    global now_playing
+    r = requests.post('http://noise:5000/stop/')
+    print("Stop button pressed.")
+    now_playing = ""
+
+def play_file(noise_name, extra_icon=""):
+    #
+    # Play the specified file
+    #
+    global now_playing, noise_pointer, display_mode
+    if noise_name == '':
+        print("Warning! Noise with no name requested.")
+        return
+    now_playing = noise_name
+    noise_pointer = wav_list.index(now_playing)
+    r = requests.post('http://noise:5000/play/' + now_playing + "/")
 
 print("Starting...")
 
@@ -100,14 +94,5 @@ while True:
         else:
             if now_playing != j["file"]:
                 now_playing = j["file"]
-                #display_now(img_list[wav_list.index(now_playing)])
-        # See if we need to save a new volume level in Redis
-        #if current_volume != new_volume:
-            # Save volume in Redis
-        #    try:
-        #        redis_conn.set('v', current_volume)
-        #   except:
-         #       print("Error saving volume data to Redis.")
-         #   new_volume = current_volume
-
+                
     time.sleep(2)
